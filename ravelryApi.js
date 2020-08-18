5f621f1859b7c31adc4a2535413620740aeacafb
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-08-18 17:47:30 -0400

    Change background o gradient, darken flip-card back, change font on card heading

--- ravelryApi.js
+++ ravelryApi.js
@@ -1,3 +1,5 @@
+// require("dotenv").config();
+
 /* API Documentation: https://www.ravelry.com/api
 
 https://www.ravelry.com/pro/sjk-designs/apps
@@ -29,18 +31,16 @@ let authUsername3 = "b047c350991e823b4b0a5e44f1957bee";
 let authPassword1 = "L58H6mGz5AN2y/qozzwXYVNls2AlKrSy/9sdJd/U";
 let authPassword2 = "gG/JEc/9GIZbW2XSGomyOZ/Q7AsV1QwbvioCDCJD";
 let authPassword3 = "mFqEQWo6VXaNQ-o9vJrvJLP6C9yl5BtrPlUxwv_Y";
-
-// let url = "https://api.ravelry.com/color_families.json"
-let url = "https://api.ravelry.com/people/phebesue75/library/search.json";
 let username = "phebesue75";
 
+// let url = `https://api.ravelry.com/people/ ${process.env.username}/library/search.json`;
+let url = `https://api.ravelry.com/people/${username}/library/search.json`;
+console.log(url);
 let rApiGet = async function (url) {
   const headers = new Headers();
   const debugFunction = this.debugFunction;
   // This is the HTTP header that you need add in order to access api.ravelry.com with a read only API key
   // `btoa` will base 64 encode a string: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
-
-  // headers.append('Authorization', 'Basic ' + btoa(this.authUsername + ":" + this.authPassword));
 
   headers.append(
     "Authorization",
@@ -61,44 +61,71 @@ let r = rApiGet(url).then((res) => {
 
 function displayData(passedData) {
   let libraryDiv = document.getElementById("library");
-  // let Heading = document.getElementById("library");
+
   let pageWrapper = document.createElement("div");
   // let libraryCount = document.createElement("h2");
   // let pages = passedData.paginator;
-  let items = passedData.volumes;
   // libraryCount.innerText = pages.page_count;
   // console.log(libraryCount);
   // libraryDiv.appendChild(pageWrapper);
   // pageWrapper.appendChild(libraryCount);
   // libraryCount.textContent = `Page Count:  ${pages.page_count}`;
-  
+  let items = passedData.volumes;
+
   items.forEach((element) => {
     console.log(element);
-    
+
     let box = document.createElement("div");
+    let inner = document.createElement("div"); /* Flip-card*/
+    let front = document.createElement("div"); /*Flip-card*/
     let img = document.createElement("img");
+    let body = document.createElement("div");
     let title = document.createElement("h5");
+    let author = document.createElement("p");
     let dateAdd = document.createElement("p");
     libraryDiv.appendChild(box);
-    box.append(img);
-    box.append(title);
-    box.append(dateAdd);
+    // box.append(img);
+    // box.append(body);
+    body.append(title);
+    body.append(author);
+    // body.append(dateAdd);
     box.classList.add("library-item");
-    box.classList.add("card");
+    // box.classList.add("card");
+
     // pageWrapper.classList.add("pageWrapper");
-    dateAdd.classList.add("library-added");
-    dateAdd.classList.add("card-text");
-    img.classList.add("library-img");
-    img.classList.add("card-img-top");
-    title.classList.add("item-title");
-    title.classList.add("card-title");
+    // img.classList.add("library-img");
+    // img.classList.add("card-img-top");
+    //  -----  tiles  -----
+    // body.classList.add("card-body");
+    // title.classList.add("item-title");
+    // title.classList.add("card-title");
+    // author.classList.add("card-text");
+    // dateAdd.classList.add("library-added");
+    // dateAdd.classList.add("card-text");
     img.src = element.square_image_url;
-    title.innerText= element.title;
+
+    // -----  Flip Card-----
+    libraryDiv.appendChild(box);
+    box.append(inner);
+    inner.append(front);
+    front.append(img);
+    inner.append(body);
+
+    box.classList.add("flip-card");
+    inner.classList.add("flip-card-inner");
+    front.classList.add("flip-card-front");
+    body.classList.add("flip-card-back");
+    title.classList.add("title");
+
+    img.src = element.square_image_url;
+
+
+    // img.style.borderRadius = "25px  25px 0 0";
+    title.innerText = element.title;
+    author.textContent = `by:  ${element.author_name}`;
     dateAdd.innerText = element.created_at;
   });
 }
-
-// 1: {created_at: "2019/12/13 10:28:45 -0500", id: 307217190, pattern_id: 987222, pattern_source_id: null, updated_at: "2019/12/13 10:28:45 -0500", …}
 
 //   RavelryApi.prototype.get = function(url) {
 // RavelryApi.prototype.get = async function(url)
